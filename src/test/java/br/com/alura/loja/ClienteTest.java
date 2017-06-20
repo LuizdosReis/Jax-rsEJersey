ed12c04927356f68ab4188493d82404cbf8e4753
luizdosreis <luizdosreis@gmail.com>
2017-06-20 20:03:19 -0300

    criando filtro e logging

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -7,6 +7,8 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -35,7 +37,9 @@ public class ClienteTest {
 	
 	@Before
 	public void criandoAlvo() {
-		Client client = ClientBuilder.newClient();
+		ClientConfig config = new ClientConfig();
+		config.register(new LoggingFilter());
+		Client client = ClientBuilder.newClient(config);
 		target = client.target("http://localhost:8080");
 	}
 	
